1d65e9821100fce07a79c50bc7c8c948dffa3d1f
RomanKitsela <roman.kitsela@ambersideenergy.com>
2025-07-23 12:24:29 +0100

    add solution to p1695

--- leetcode/solutions/p1695_maximum_erasure_value.py
+++ leetcode/solutions/p1695_maximum_erasure_value.py
@@ -4,8 +4,70 @@ from typing import List
 
 class Solution:
     def maximumErasureValue(self, nums: List[int]) -> int:
-        # TODO: implement the solution
-        pass
+        
+        """
+        Solution might work but be too inefficient.
+        """
+        
+        # [v, -, -, v, -]
+        # [a, b, c, d, e]
+
+        array_len = len(nums)
+        left_idx = 0
+        best_so_far = nums[0]
+        int_set = set([nums[0]])
+        current_sum = nums[0]
+
+        # move the right idx
+
+        for right_idx in range(left_idx+1, array_len):
+            
+            new_num = nums[right_idx]
+
+            # check if new number is a duplicate
+
+            if new_num in int_set:
+                # encountered a duplicate
+                # need to move the left index to the right until we encounter the same number
+                
+                # encountered a duplicate
+                # [v, -, -, -, -, v]
+                # [1, 2, 3, 4, 5, 3]
+                # left = 1
+                # int_set = {1, 2, 3, 4, 5}
+                
+                # [-, v, -, -, -, v]
+                # [1, 2, 3, 4, 5, 3]
+                # left = 2
+                # int_set = {2, 3, 4, 5}
+                
+                # [-, -, v, -, -, v]
+                # [1, 2, 3, 4, 5, 3]
+                # left = 3
+                # int_set = {3, 4, 5}
+                
+                # [1, 2, 3, 4, 5]
+                while nums[left_idx] != new_num:
+                    int_set.remove(nums[left_idx])
+                    current_sum -= nums[left_idx]
+                    left_idx += 1
+                
+                # current sum stays the same
+                left_idx += 1
+                
+            else:
+                # not a duplicate
+                
+                # add to int set
+                # add to current sum
+                
+                int_set.add(new_num)
+                current_sum += new_num
+                
+                if current_sum > best_so_far:
+                    best_so_far = current_sum
+
+        return best_so_far
 
 
 if __name__ == "__main__":
--- leetcode/tests/test_p1695.py
+++ leetcode/tests/test_p1695.py
@@ -10,14 +10,13 @@ class TestMaximumErasureValue(unittest.TestCase):
 
     def test_examples(self):
         # LeetCode examples
-        self.run_test([4,2,4,5,6], None)
-        self.run_test([5,2,1,2,5,2,1,2,5], None)
+        self.run_test([4,2,4,5,6], 17)
+        self.run_test([5,2,1,2,5,2,1,2,5], 8)
 
     def test_edge_cases(self):
         # Your own boundary / random tests
-        self.run_test([], None)
-        self.run_test([100], None)
-        self.run_test([1,1,1,1], None)
+        self.run_test([100], 100)
+        self.run_test([1,1,1,1], 1)
 
     # Add more as you like…
 
