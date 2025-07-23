e433542911c8b5f5e1b801b8c1f798977f11598a
RomanKitsela <roman.kitsela@ambersideenergy.com>
2025-07-23 12:29:51 +0100

    Use prefix-sum + last-seen index for O(n) max unique subarray

--- leetcode/solutions/p1695_maximum_erasure_value.py
+++ leetcode/solutions/p1695_maximum_erasure_value.py
@@ -4,70 +4,28 @@ from typing import List
 
 class Solution:
     def maximumErasureValue(self, nums: List[int]) -> int:
-        
-        """
-        Solution might work but be too inefficient.
-        """
-        
-        # [v, -, -, v, -]
-        # [a, b, c, d, e]
+        n = len(nums)
+        P = [0] * (n + 1)
+        for i, x in enumerate(nums):
+            P[i+1] = P[i] + x
 
-        array_len = len(nums)
-        left_idx = 0
-        best_so_far = nums[0]
-        int_set = set([nums[0]])
-        current_sum = nums[0]
+        MAXV = 10000
+        last = [-1] * (MAXV + 1)
+        ans = 0
+        left = 0
 
-        # move the right idx
+        for i, x in enumerate(nums):
+            j = last[x]
+            if j >= left:
+                left = j + 1
 
-        for right_idx in range(left_idx+1, array_len):
-            
-            new_num = nums[right_idx]
+            curr = P[i+1] - P[left]
+            if curr > ans:
+                ans = curr
 
-            # check if new number is a duplicate
+            last[x] = i
 
-            if new_num in int_set:
-                # encountered a duplicate
-                # need to move the left index to the right until we encounter the same number
-                
-                # encountered a duplicate
-                # [v, -, -, -, -, v]
-                # [1, 2, 3, 4, 5, 3]
-                # left = 1
-                # int_set = {1, 2, 3, 4, 5}
-                
-                # [-, v, -, -, -, v]
-                # [1, 2, 3, 4, 5, 3]
-                # left = 2
-                # int_set = {2, 3, 4, 5}
-                
-                # [-, -, v, -, -, v]
-                # [1, 2, 3, 4, 5, 3]
-                # left = 3
-                # int_set = {3, 4, 5}
-                
-                # [1, 2, 3, 4, 5]
-                while nums[left_idx] != new_num:
-                    int_set.remove(nums[left_idx])
-                    current_sum -= nums[left_idx]
-                    left_idx += 1
-                
-                # current sum stays the same
-                left_idx += 1
-                
-            else:
-                # not a duplicate
-                
-                # add to int set
-                # add to current sum
-                
-                int_set.add(new_num)
-                current_sum += new_num
-                
-                if current_sum > best_so_far:
-                    best_so_far = current_sum
-
-        return best_so_far
+        return ans
 
 
 if __name__ == "__main__":
